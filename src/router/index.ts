7b0d3f17b0238a109984f2b6b2b7336b181db213
Andrew Gomes <andrewgomes1328@gmail.com>
2024-04-13 12:25:51 -0300

    funcionalidade para cadastrar aula funcionando porém sem css e melhorias no código pode ser feita

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import loginVue from "../pages/login.vue";
 import roadmapVue from "../pages/roadmap.vue";
 import cursosVue from "../pages/cursos.vue";
 import matriculaVue from "../pages/matricula.vue";
+import CadastrarAula from "../pages/cadastrarAula.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -43,6 +44,12 @@ const router = createRouter({
       path: "/matricula",
       name: "matricula",
       component: matriculaVue,
+      meta: { requiresAuth: false },
+    },
+    {
+      path: "/admin/cadastro/aula",
+      name: "cadastrarAula",
+      component: CadastrarAula,
       meta: { requiresAuth: false },
     }
   ],
